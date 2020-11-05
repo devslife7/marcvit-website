9398bceabed9696a0ba89752728517d91fa1240a
Marcos Velasco <devslife7@gmail.com>
2020-11-05 00:54:38 -0500

    fixing yarn start

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,13 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 import Video from "../../videos/video.mp4"
-import { HeroBg, HeroContainer, VideoBg } from "./HeroElements"
+// import { Button } from "../ButtonElement"
+import {
+  ArrowForward,
+  ArrowRight,
+  HeroBg,
+  HeroBtnWrapper,
+  HeroContainer,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  VideoBg,
+} from "./HeroElements"
 
 export default function HeroSection() {
+  const [hover, setHover] = useState(false)
+
+  const onHover = () => {
+    setHover(!hover)
+  }
+
   return (
     <HeroContainer id='Home'>
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
       </HeroBg>
+      <HeroContent>
+        <HeroH1>Virtual Bankin Made Easy</HeroH1>
+        <HeroP>Sign up for a new account today and receive $250 in credit towards your next payment</HeroP>
+        <HeroBtnWrapper>
+          <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover}>
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   )
 }
