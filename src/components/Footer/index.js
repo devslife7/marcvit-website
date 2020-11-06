a7092e2b21a153fda84d8ee787c2c72983167792
Marcos Velasco <devslife7@gmail.com>
2020-11-06 01:28:39 -0500

    finished footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin } from "react-icons/fa"
 import {
   FooterContainer,
   FooterLink,
@@ -7,6 +8,12 @@ import {
   FooterLinksWrapper,
   FooterLinkTitle,
   FooterWrap,
+  SocialIconLink,
+  SocialIcons,
+  SocialLogo,
+  SocialMedia,
+  SocialMediaWrap,
+  WebsiteRights,
 } from "./FooterElements"
 
 export default function Footer() {
@@ -51,6 +58,30 @@ export default function Footer() {
             </FooterLinkItems>
           </FooterLinksWrapper>
         </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to='/'>dolla</SocialLogo>
+            <WebsiteRights>dolla © {new Date().getFullYear()} All rights reserved.</WebsiteRights>
+
+            <SocialIcons>
+              <SocialIconLink href='/' target='_blank' arial-label='Facebook'>
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' arial-label='Instagram'>
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' arial-label='Youtube'>
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' arial-label='Twitter'>
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' arial-label='LinkedIn'>
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
       </FooterWrap>
     </FooterContainer>
   )
